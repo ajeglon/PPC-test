d65cff05f6180f4eaa0cfe2a4ebca183b833fbd4
ajeglon <61421392+ajeglon@users.noreply.github.com>
2020-10-20 13:19:01 +0100

    got route working and other fixes

--- app/routes.js
+++ app/routes.js
@@ -15,19 +15,19 @@ var benificiary = {
 // Add your routes here - above the module.exports line
 
 // Branching - post the url of the page you want to branch from
-router.post('/certificate-how-to-pay-for', function (req, res) {
+router.post('/certificate', function (req, res) {
   // Create a variable using the name of the select of radio button value you want to store
-  var install = req.session.data['contact']
+  var install = req.session.data['months']
   //if statemennt to branch based on conditional values
-  if (install == "email"){
+  if (install == "three-months"){
     res.redirect('/certificate-prepayment')
   }
-  if (install == "phone"){
-    res.redirect('/certificate-prepayment')
+  if (install == "twelve-months"){
+    res.redirect('/certificate-how-to-pay-for-twelve-months')
   }
   //else might not be necessary
   else {
-    res.redirect('/certificate-prepayment')
+    res.redirect('/certificate')
   }
 
 })
